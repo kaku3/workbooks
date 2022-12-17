93b3e6d97f3c87efb2adc1bff6b68b0ae7038ba9
kaku3 <kodakaku@gmail.com>
2022-12-17 16:55:36 +0900

    feat: タイトル、メーカフィルタ

--- 03_switch_eshop/10_src/nintendo-eshop/components/GameList.vue
+++ 03_switch_eshop/10_src/nintendo-eshop/components/GameList.vue
@@ -1,27 +1,60 @@
 <!-- Please remove this file from your project -->
 <template>
   <div>
+    <v-row>
+      <v-col cols="6">
+        <v-text-field
+          v-model="searchTitle"
+          label="タイトル"
+          hide-details
+          dense
+        />
+      </v-col>
+      <v-col cols="6">
+        <v-autocomplete
+          v-model="selectedMakers"
+          :items="makers"
+          label="メーカー"
+          multiple
+          chips
+          small-chips
+          dense
+        />
+      </v-col>
+    </v-row>
     <v-data-table
       :headers="gameHeaders"
-      :items="games"
-      height="80vh"
+      :items="filteredGames"
+      height="calc(100vh - 216px)"
       fixed-header
       hide-default-footer
       disable-pagination
+      dense
     >
       <template #[`item.TitleName`]="{ item }">
-        {{ item.TitleName }}
+        <div class="d-flex __clickable" @click="onShowPage(item)">
+          <v-img
+            :src="item.ScreenshotImgURL"
+            max-width="120"
+          />
+          <div>
+            {{ item.TitleName }}
+          </div>
+        </div>
       </template>
     </v-data-table>
   </div>
 </template>
 
 <script>
+const BASE_URL = 'https://store-jp.nintendo.com/list/software/'
+
 export default {
   name: 'GameList',
   data: () => {
     const games = require('~/assets/data/games.json')
       .map((g) => {
+        g.TitleName = String(g.TitleName)
         g._price = Number(g.Price.replace(/[^0-9a-z]/gi, ''))
         return g
       })
@@ -30,7 +63,10 @@ export default {
 
     return {
       games,
-      makers
+      makers,
+      searchTitle: '',
+      selectedMakers: [],
+      selectedGame: null
     }
   },
   computed: {
@@ -38,10 +74,37 @@ export default {
       return [
         { text: 'タイトル', value: 'TitleName' },
         { text: 'メーカー', value: 'MakerName' },
-        { text: '価格', value: '_price' },
-        { text: '発売日', value: 'SalesDate' }
+        { text: '価格', value: '_price', align: 'end' },
+        { text: '発売日', value: 'SalesDate', align: 'end' }
       ]
+    },
+    filteredGames () {
+      let games = this.games
+      if (this.selectedMakers.length > 0) {
+        games = games.filter(g => this.selectedMakers.includes(g.MakerName))
+      }
+      if (this.searchTitle !== '') {
+        games = games.filter(g => ~g.TitleName.indexOf(this.searchTitle))
+      }
+      return games
+    },
+    showPageUrl () {
+      if (!this.selectedGame) {
+        return ''
+      }
+      return BASE_URL + this.selectedGame.LinkURL.replace('/titles/', '') + '.html'
+    }
+  },
+  methods: {
+    onShowPage (game) {
+      this.selectedGame = game
+      window.open(this.showPageUrl, '_blank')
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.__clickable {
+  cursor: pointer;
+}
+</style>
