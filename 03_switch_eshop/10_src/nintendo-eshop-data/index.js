99c50a024849325373f9385544040732b9886394
kaku3 <kodakaku@gmail.com>
2022-12-18 13:43:09 +0900

    feat: 画像取得追加

--- 03_switch_eshop/10_src/nintendo-eshop-data/index.js
+++ 03_switch_eshop/10_src/nintendo-eshop-data/index.js
@@ -1,8 +1,25 @@
+// eshop 情報収集ツール
+// 【使い方】
+// node index.js
+// 
+// {OUT_FOLDER} にゲーム一覧、各ゲーム詳細情報を出力
+//   games.json             # ゲーム一覧
+//   details/
+//     {InitialCode}.json   # 各ゲーム詳細情報
+//
+// {THUMBNAIL_FOLDER} にサムネイルを保存
+//   {InitialCode}.jpg      # 320x サムネイル
+
 const fs = require('fs')
 const { getGamesJapan } = require('nintendo-switch-eshop');
 const got = require('got');
 const cheerio = require('cheerio');
 
+const axios = require('axios');
+const sharp = require('sharp');
+
+const OUT_FOLDER = '../nintendo-eshop/assets/eshop/'
+const THUMBNAIL_FOLDER = '../nintendo-eshop/static/images/eshop/games/'
 const BASE_URL = 'https://store-jp.nintendo.com/list/software/';
 
 function gameUrl(game) {
@@ -12,18 +29,17 @@ function gameUrl(game) {
 // ゲーム一覧取得
 async function getGames() {
   const games = await getGamesJapan();
-  fs.writeFileSync('./data/games.json', JSON.stringify(games), 'utf-8');
+  fs.writeFileSync(OUT_FOLDER + 'games.json', JSON.stringify(games), 'utf-8');
   return games;
 }
 
-// ゲーム情報詳細取得
+// ゲーム詳細情報取得
 // ※存在しないファイルだけ取得
-async function getGameDetail(game) {
-  console.log(`+ getGameDetail(${game.InitialCode}, ${game.TitleName})`)
-  const file = `./data/details/${game.InitialCode}.json`;
+async function getGameDetail(i, game) {
+  const file = `${OUT_FOLDER}details/${game.InitialCode}.json`;
 
   if(fs.existsSync(file)) {
-    console.log(`- skip : getGameDetail(${game.InitialCode}, ${game.TitleName})`);
+    console.log(`# skip : getGameDetail(${i}, ${game.InitialCode}, ${game.TitleName})`);
     return;
   }
 
@@ -51,17 +67,41 @@ async function getGameDetail(game) {
     players
   }
   fs.writeFileSync(file, JSON.stringify(o), 'utf-8');
-  console.log(`- saved : getGameDetail(${game.InitialCode}, ${game.TitleName})`);
+  console.log(`# saved : getGameDetail(${i}, ${game.InitialCode}, ${game.TitleName})`);
 }
 
-// game 一覧を取得して保存
+// ゲームサムネイル取得
+// ※存在しないファイルだけ取得
+async function getGameThumbnail(i, game) {
+  const file = THUMBNAIL_FOLDER + game.InitialCode + '.jpg';
+
+  if(fs.existsSync(file)) {
+    console.log(`# skip : getGameThumbnail(${i}, ${game.InitialCode}, ${game.TitleName})`);
+    return;
+  }
+
+  // screen shot を取得してリサイズして保存
+  const res = await axios.get(game.ScreenshotImgURL, { responseType: 'arraybuffer' });
+  const thumbnail = await sharp(res.data)
+    .resize(320)
+    .jpeg()
+    .toBuffer();
+  fs.writeFileSync(file, thumbnail, 'binary');
+  console.log(`# saved : getGameThumbnail(${i}, ${game.InitialCode}, ${game.TitleName})`);
+}
+
 async function main() {
+  // ゲーム一覧取得
   const games = await getGames()
   console.log(games.length);
+
+  // ゲーム詳細情報取得
   for(let i in games) {
-    console.log(i)
-    await getGameDetail(games[i])
+    await getGameDetail(i, games[i]);
+  }
+  // サムネイル画像取得
+  for(let i in games) {
+    await getGameThumbnail(i, games[i]);
   }
 }
-
 main();
