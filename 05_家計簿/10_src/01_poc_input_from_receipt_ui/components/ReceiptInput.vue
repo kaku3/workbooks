ea2a6083fb5b880cb7e002781576078260ccadb8
kaku3 <kodakaku@gmail.com>
2023-01-10 00:24:11 +0900

    feat: レスポンシブ対応

--- 05_家計簿/10_src/01_poc_input_from_receipt_ui/components/ReceiptInput.vue
+++ 05_家計簿/10_src/01_poc_input_from_receipt_ui/components/ReceiptInput.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="ReceiptInput">
     <v-row>
-      <v-col cols="6">
-        <v-img
-          :src="receiptImage"
+      <v-col md="6" sm="12">
+        <div
+          class="receipt-image-container"
+          :style="receiptImageContainerStyle"
         >
-        </v-img>
+          <v-img
+            :src="receiptImage"
+          />
+        </div>
       </v-col>
-      <v-col cols="6">
-        <v-list-item-group>
+      <v-col md="6" sm="12">
+        <v-list-item-group
+          :style="paragraphContainerStyle"
+        >
           <v-list-item v-for="(paragraph, i) in paragraphs" :key="i">
             <v-chip v-for="(c, j) in paragraph" :key="j"
               outlined
@@ -20,6 +26,60 @@
             </v-chip>
           </v-list-item>
         </v-list-item-group>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col md="2" sm="6">
+
+        <v-menu
+          ref="menu"
+          v-model="ui.showDatePicker"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="ui.date"
+              label="日付"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+              dense
+              hide-details
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="ui.date"
+            :active-picker.sync="ui.activePicker"
+            :max="(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)"
+            min="2020-01-01"
+            @change="save"
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+      <v-col md="2" sm="6">
+        <v-text-field
+          label="いくら"
+          dense
+          hide-details
+        />
+      </v-col>
+      <v-col md="3" sm="12">
+        <v-text-field
+          label="どこで"
+          dense
+          hide-details
+        />
+      </v-col>
+      <v-col md="5" sm="12">
+        <v-text-field
+          label="メモ"
+          dense
+          hide-details
+        />
       </v-col>
     </v-row>
   </div>
@@ -34,7 +94,12 @@ export default {
     return {
       receiptImage: require('@/assets/receipt/receipt.jpg'),
       receiptInformation,
-      textAnnotations
+      textAnnotations,
+      ui: {
+        showDatePicker: false,
+        activePicker: null,
+        date: null,
+      }
     }
   },
   mounted () {
@@ -51,21 +116,37 @@ export default {
           const texts = []
           for(const word of paragraph.words) {
             const text = word.symbols.reduce((a, v) => a += v.text, "")
-            texts.push(text);
+            texts.push(text)
           }
-          items.push(texts);
+          items.push(texts)
         }
       }
-      return items;
+      return items
+    },
+    receiptImageContainerStyle () {
+      return {
+        height: this.$vuetify.breakpoint.mobile ? '30vh' : '70vh'
+      }
+    },
+    paragraphContainerStyle () {
+      return {
+        height: this.$vuetify.breakpoint.mobile ? 'calc(60vh - 168px)' : '70vh'
+      }
     }
   },
   methods: {
+    save (date) {
+      // TODO
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
+
+.receipt-image-container {
+  overflow: auto;
+}
 .v-list-item-group {
-  height: 80vh;
   overflow: auto;
   .v-list-item {
     padding-top: .25rem;
