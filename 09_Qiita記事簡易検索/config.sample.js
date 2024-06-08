1c4a10b7a9e295d3624b329c64c1b9af3968fbb8
kaku3 <kodakaku@gmail.com>
2024-06-08 18:36:37 +0900

    docs: Qiita簡易検索。記事取得

---
+++ 09_Qiita記事簡易検索/config.sample.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // https://qiita.com/settings/applications
+  // 個人用アクセストークンを作成して貼り付け。
+  token: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+}
---
+++ 09_Qiita記事簡易検索/index.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const config = require('./config');
+
+function getUserArticlesUrl(user, page) {
+  return `https://qiita.com/api/v2/users/${user}/items?page=${page}&per_page=100`;
+}
+
+const articles = [];
+
+async function fetch(token, user) {
+  console.group('+ fetch', token, user);
+  for(let page = 1; page <= 5; page++) {
+    const url = getUserArticlesUrl(user, page);    
+
+    const res = await axios.get(url, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    const items = res.data;
+
+    console.info(url, items.length);
+
+    if(items.length > 0) {
+      articles.push(...items);
+    } else {
+      break;
+    }
+  }
+  console.groupEnd();
+  console.info(articles.length);
+  fs.writeFileSync(
+    'data/articles.js',
+    'const ARTICLES = ' + JSON.stringify(articles, null, 2),
+    'utf-8'
+  );
+}
+
+(async () => {
+  await fetch(config.token, 'kaku3');
+})();
